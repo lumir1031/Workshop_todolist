6b6a90f90c553bd6b7ac47722aa627fea0996a61
eason <acontentedmind@gmail.com>
2020-12-08 16:22:59 +0800

    check

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -6,56 +6,6 @@ window.addEventListener('DOMContentLoaded', function(){
     list.classList.toggle('checked')
   }))
  
-  // let cross = document.querySelectorAll('.close')
-  // for(let i = 0; i< list.length; i++){
-  //   cross[i].addEventListener('click', function(){
-  //     list[i].remove()
-  //   })
-  // }
-
-
-  let cross = document.querySelectorAll('.close')
-  cross.forEach(cross => cross.addEventListener('click', function(event){
-    event.target.parentNode.remove()
-  }))
-
-
-  document.getElementById('addBtn').addEventListener('click', function(){
-    let input = document.getElementById('input').value
-    const span = document.createElement('span')
-    const li = document.createElement('li')
-    span.className = 'close'
-    span.textContent = 'x'
-    li.textContent = input
-    li.insertAdjacentElement('beforeend', span)
-    if(input != ''){
-      document.querySelector('ul').insertAdjacentElement('beforeend', li)
-      console.log(input)
-      document.getElementById('input').value = ""
-    }
-
-
-
-
-
-    li.addEventListener('click', function(){
-      li.classList.toggle('checked')
-    })
-
-
-    // let list = document.querySelectorAll('li')
-    // list.forEach(list => list.addEventListener('click',function(){
-    //   list.classList.toggle('checked')
-    // }))
-
-
-
-    let cross = document.querySelectorAll('.close')
-    cross.forEach(cross => cross.addEventListener('click', function(event){
-      event.target.parentNode.remove()
-    }))
-
-  })
 })
     
     
