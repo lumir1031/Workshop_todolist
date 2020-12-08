1d932a25c087a9322ffd6d0353a98387b853447f
bobanderic2000 <bobanderic2000@gmail.com>
2020-12-08 14:29:17 +0800

    feature/delete_completed

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,19 +1,26 @@
 // TO DO
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   // 1-forEach寫法
-  // const list document.querySelectorAll('li')
-  // list.forEach()
-
+  const list = document.querySelectorAll('li')
+  list.forEach(item => item.addEventListener('click', function() {
+    item.classList.toggle('checked')
+  })
+  )
   // 1-for 迴圈寫法
-  const list = document.querySelectorAll('li');
-    for(let i=0; i<list.length; i++) {
-      list[i].addEventListener('click', function() {
-      list[i].classList.toggle('checked')
-      })
-    }
+  // for (let i = 0; i < list.length; i++) {
+  //   list[i].addEventListener('click', function () {
+  //     list[i].classList.toggle('checked')
+  //   })
+  // }
 
-  // const x = document.querySelector('close')
-  // x.addEventListener('click', function() {
-  
-  // })
+  // 2-for 迴圈寫法
+  const allx = document.querySelectorAll('.close')
+  for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function () {
+      list[i].classList.toggle('checked')
+    })
+    allx[i].addEventListener('click', function() {
+      list[i].remove()
+    })
+  }
 })
