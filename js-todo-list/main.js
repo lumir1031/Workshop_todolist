968dee5b8ead770ec7442772f1ff891eacbcca25
MarlboroYang <yangmarlboro@gmail.com>
2020-12-08 15:29:35 +0800

    add checked

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,7 @@
 // TO DO
+document.addEventListener('DOMContentLoaded', function(){
+  const list = document.querySelector('ul')
+  list.addEventListener('click', function(e){
+    e.target.classList.toggle('checked')
+  })
+})
