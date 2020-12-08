4ea3a66083a111da8471202aa06056f8747bf3ad
eason <acontentedmind@gmail.com>
2020-12-08 16:26:14 +0800

    delete

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -6,6 +6,21 @@ window.addEventListener('DOMContentLoaded', function(){
     list.classList.toggle('checked')
   }))
  
+  // let cross = document.querySelectorAll('.close')
+  // for(let i = 0; i< list.length; i++){
+  //   cross[i].addEventListener('click', function(){
+  //     list[i].remove()
+  //   })
+  // }
+
+
+  let cross = document.querySelectorAll('.close')
+  cross.forEach(cross => cross.addEventListener('click', function(event){
+    event.target.parentNode.remove()
+  }))
+
+
+
 })
     
     
