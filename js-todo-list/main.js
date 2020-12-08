051817884ca5824d21cf3f175ba5442871ba9091
bobanderic2000 <bobanderic2000@gmail.com>
2020-12-08 12:09:38 +0800

    feature/click_li_completed

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,8 @@
 // TO DO
+window.addEventListener('DOMContentLoaded', function() {
+  const item = document.querySelector('li');
+  item.addEventListener('click', function() {
+    const item = document.querySelectorAll('li');
+    item.classList.toggle('.checked');
+  })
+})
