41ebe8756dc468b00482c2501e015cf3b80b5661
bobanderic2000 <bobanderic2000@gmail.com>
2020-12-08 13:08:35 +0800

    correct-for-circle

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,8 +1,19 @@
 // TO DO
 window.addEventListener('DOMContentLoaded', function() {
-  const item = document.querySelector('li');
-  item.addEventListener('click', function() {
-    const item = document.querySelectorAll('li');
-    item.classList.toggle('.checked');
-  })
+  // 1-forEach寫法
+  // const list document.querySelectorAll('li')
+  // list.forEach()
+
+  // 1-for 迴圈寫法
+  const list = document.querySelectorAll('li');
+    for(let i=0; i<list.length; i++) {
+      list[i].addEventListener('click', function() {
+      list[i].classList.toggle('checked')
+      })
+    }
+
+  // const x = document.querySelector('close')
+  // x.addEventListener('click', function() {
+  
+  // })
 })
