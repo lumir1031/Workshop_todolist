46cb218db1e26157f9e7247d34168408184fb81a
eason <acontentedmind@gmail.com>
2020-12-08 13:56:14 +0800

    list_finish

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,33 @@
 // TO DO
+window.addEventListener('DOMContentLoaded', function(){
+
+  let list = document.querySelectorAll('li')
+  list.forEach(list => list.addEventListener('click',function(){
+    list.classList.toggle('checked')
+  }))
+
+  let cross = document.querySelectorAll('.close')
+  cross.forEach(cross => cross.addEventListener('click',function(){
+    document.querySelector('li').remove()
+  }))
+
+
+  document.getElementById('addBtn').addEventListener('click', function(){
+    let input = document.getElementById('input').value
+
+
+    const span = document.createElement('span')
+    span.className = 'close'
+    span.textContent = 'x'
+
+    
+    const div = document.createElement('li')
+    
+    div.insertAdjacentElement('beforeend', span)
+    
+    div.textContent = input
+    
+    document.querySelector('ul').insertAdjacentElement('beforeend', div)
+  })
+  
+})
