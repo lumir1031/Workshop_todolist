c8a368330ca10385c036839e4d800e8b0250197a
Fish Kuo <fishkuo0101@gmail.com>
2020-12-08 11:44:31 +0800

    add check-uncheck

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,10 @@
 // TO DO
+
+// items-completed
+window.addEventListener('DOMContentLoaded', function () {
+	let items = document.querySelectorAll('ul li').forEach((item) => {
+		item.addEventListener('click', function () {
+			item.classList.toggle('checked');
+		});
+	});
+});
