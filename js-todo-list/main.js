0cf4727fde89e04d75fcee1f394ea452674ce7de
Alex Chuang <ball16357@gmail.com>
2021-04-20 11:55:22 +0800

    第一條checked

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,14 @@
-// TO DO
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('li').addEventListener('click', function() {
+    console.log('123')
+
+    var checkLi = document.createElement('li')
+    this.classList.toggle('checked')
+
+
+  document.querySelector('li').addEventListener('click', function() {
+    var checkLi = document.createElement('li')
+    this.classList.toggle('checked')
+  })
+  })
+})
