c049d6bc14f9fe503b29765b40d2d54c6973fef2
Noel Hsiao <huihsinhsiao12@gmail.com>
2021-04-20 11:48:24 +0800

    add no.1 功能

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,5 @@
-// TO DO
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('ul').addEventListener('click', function(e) {
+    e.target.classList.toggle('checked');
+  })
+})
