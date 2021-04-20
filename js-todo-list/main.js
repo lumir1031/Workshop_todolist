4069b4d92102cd7c599bddcc335daeb774b6bdef
YCChen <t5204713910@gmail.com>
2021-04-20 14:58:08 +0800

    function_to_add_new_element

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,19 +1,5 @@
 // TO DO
 document.addEventListener('DOMContentLoaded', function () {
-  const toDoElements = document.querySelectorAll('li').forEach(
-    li => {
-      li.addEventListener('click', () => {
-        li.classList.toggle("checked");
-      });
-    }
-  );
-  const toDoDeletes = document.querySelectorAll('span').forEach(
-    span => {
-      span.addEventListener('click', () => {
-        span.closest("li").remove();
-      });
-    }
-  );
   document.getElementById('addBtn').addEventListener('click', function() {
     let toDoMessage = document.getElementById('input').value;
     let ul = document.getElementById('ul');
@@ -25,7 +11,34 @@ document.addEventListener('DOMContentLoaded', function () {
       li.textContent = toDoMessage;
       li.appendChild(span);
       document.querySelector('ul').appendChild(li);
+    };
+    let toDoDeletes = document.querySelectorAll('span').forEach(
+      span => {
+        span.addEventListener('click', () => {
+          span.closest("li").remove();
+        });
+      }
+    );
+    let toDoElements = document.querySelectorAll('li').forEach(
+      li => {
+        li.addEventListener('click', () => {
+          li.classList.toggle("checked");
+        });
+      }
+    );
+  });
+  let toDoElements = document.querySelectorAll('li').forEach(
+    li => {
+      li.addEventListener('click', () => {
+        li.classList.toggle("checked");
+      });
     }
-  })
-
-})
+  );
+  let toDoDeletes = document.querySelectorAll('span').forEach(
+    span => {
+      span.addEventListener('click', () => {
+        span.closest("li").remove();
+      });
+    }
+  );
+});
