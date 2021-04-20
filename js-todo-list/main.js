7e7da63391c793df8cb08484fb69e75d6c19f37c
Alex Chuang <ball16357@gmail.com>
2021-04-20 19:41:18 +0800

    新增含文字的list

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -10,4 +10,15 @@ document.addEventListener('DOMContentLoaded', function() {
       e.target.parentElement.remove()
     })
   })
+
+  document.getElementById('addBtn').addEventListener('click', function(ea){
+    const value = ea.target.previousElementSibling.value
+    const newLi = document.createElement('li')    
+    newLi.classList.add('span')
+    document.querySelector('ul').appendChild(newLi).innerHTML = value + '<span class="close">x</span>'
+    
+    newLi.lastChild.addEventListener('click', function(){
+      newLi.remove()
+    })
+  })
 })
