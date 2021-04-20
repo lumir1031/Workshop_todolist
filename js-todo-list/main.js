8bad25e929a5222916bd40aedc6e904e099f1d94
Alex Chuang <ball16357@gmail.com>
2021-04-20 14:00:03 +0800

    刪除list功能

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -2,4 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('ul').addEventListener('click', function(event) {
     event.target.classList.toggle('checked')
   })
+
+
+  const el = document.querySelectorAll('.close')
+  el.forEach(function(e){
+    e.addEventListener('click', function(e) {
+      e.target.parentElement.remove()
+    })
+  })
 })
