a4c4c56d56a026da941196c7ace32f8f481b907f
p4952015 <p4952015@yahoo.com.tw>
2021-04-20 16:24:06 +0800

    add remove

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,25 +1,21 @@
 // TO DO
-function checked(){
+document.addEventListener('DOMContentLoaded', function(){
   let li = document.querySelectorAll('li');
-
+  let close = document.querySelectorAll('.close');
+  let input = document.querySelector('#input');
+  let addBtn = document.querySelector('#addBtn');
+  
+  // checked
   for(let i = 0; i < li.length; i++){
     li[i].addEventListener('click', function(){
       this.classList.add('checked');
     })
   }
-}
-checked();
-
-function remove(){
-  let close = document.querySelectorAll('.close');
-  // console.log(close);
+  // remove
   for(let i = 0; i < close.length; i++){
     close[i].addEventListener('click', function(){
       this.parentElement.remove();
     })
   }
-}
-remove();
 
-
-
+})
