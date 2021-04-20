c0a00c6159eaae14c268ee2d3aa46d56c3fd0ca7
p4952015 <p4952015@yahoo.com.tw>
2021-04-20 13:29:34 +0800

    完成checked

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,12 @@
 // TO DO
+function checked(){
+  let li = document.querySelectorAll('li');
+
+  for(i = 0; i < li.length; i++){
+    li[i].addEventListener('click', function(){
+      this.classList.add('checked');
+    })
+  }
+
+}
+checked();
