47400e69b5b54ed1e592b0b1de98978e02852c47
linda <linlin60528@gmail.com>
2021-04-20 16:03:26 +0800

    點選

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,8 @@
-// TO DO
+document.addEventListener('DOMContentLoaded',function(){
+  document.querySelectorAll('li').forEach(function(list){
+    list.addEventListener('click', function(e){
+      e.target.classList.toggle('checked')
+    });
+  })
+})
+  
