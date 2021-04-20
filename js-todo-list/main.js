2c4d6989ddb767961e7a459f64dcffb38777ac3d
SunWei Chou <sunwei8524@gmail.com>
2021-04-20 15:12:31 +0800

    add newTask

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -2,12 +2,39 @@ document.addEventListener('DOMContentLoaded', function(){
   const list = document.querySelectorAll('li')
   const span = document.querySelectorAll('li > span')
   
+  //完成功能
   list.forEach(ele => ele.addEventListener('click', function(event){
     event.target.classList.toggle('checked') 
   }))
 
+  //刪除功能
   span.forEach(ele => ele.addEventListener('click', function(event){
     event.target.parentNode.remove()
   }))
 
+  //新增功能
+  document.querySelector('#addBtn').addEventListener('click', function(){
+    const inputTask = document.querySelector('#input')
+    const newTask = document.createElement('li')
+    const close = document.createElement('span')
+    newTask.textContent = inputTask.value
+    close.textContent = 'x'
+    close.classList.add('close')
+
+    console.log(inputTask.value);
+    newTask.insertAdjacentElement('beforeend', close)
+
+    newTask.addEventListener('click', function(event){
+      event.target.classList.toggle('checked')
+    })
+    close.addEventListener('click', function(event){
+      event.target.parentNode.remove()
+    })
+    
+    if (inputTask.value != ''){
+      document.querySelector('ul').insertAdjacentElement('beforeend', newTask)
+      inputTask.value = ''
+    }
+    
+  })
 })
