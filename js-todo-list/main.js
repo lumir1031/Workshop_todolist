93d10b7beb6b8e6f82190be3a00e67a9a8a43659
SunWei Chou <sunwei8524@gmail.com>
2021-04-20 11:52:29 +0800

    add checked

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,5 @@
-// TO DO
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('ul').addEventListener('click', function(event){
+    event.target.classList.toggle('checked')
+  })
+})
