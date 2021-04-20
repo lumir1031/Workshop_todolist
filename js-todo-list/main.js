66fca3883d0138bc8b212bc82f47b98c2c68fc68
TANG <tyu4520@gmail.com>
2021-04-20 12:06:18 +0800

    complete add check

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,6 @@
-// TO DO
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('ul').addEventListener('click', function(e){
+    // debugger
+    e.target.classList.toggle('checked')
+  })
+})
