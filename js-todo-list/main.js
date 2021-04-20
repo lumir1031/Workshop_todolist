bf8281565e8e400c720d237071163bdd0deac88e
linda <linlin60528@gmail.com>
2021-04-20 16:13:26 +0800

    delete

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -2,7 +2,12 @@ document.addEventListener('DOMContentLoaded',function(){
   document.querySelectorAll('li').forEach(function(list){
     list.addEventListener('click', function(e){
       e.target.classList.toggle('checked')
-    });
-  })
+    })
+  });
+    document.querySelectorAll('span').forEach(function(list){
+      list.addEventListener('click', function(e){
+        e.target.parentNode.remove()
+      })
+    }
 })
   
