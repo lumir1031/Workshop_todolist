09d408cada0c500bdad98475fed40e079785efd0
Alex Chuang <ball16357@gmail.com>
2021-04-20 12:16:06 +0800

    切換list功能

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,14 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('li').addEventListener('click', function() {
-    console.log('123')
-
-    var checkLi = document.createElement('li')
-    this.classList.toggle('checked')
-
-
-  document.querySelector('li').addEventListener('click', function() {
-    var checkLi = document.createElement('li')
-    this.classList.toggle('checked')
-  })
+  document.querySelector('ul').addEventListener('click', function(event) {
+    event.target.classList.toggle('checked')
   })
 })
