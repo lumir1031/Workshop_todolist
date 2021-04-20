553792df5f4f527418d3f065e58536d3c27ba655
SunWei Chou <sunwei8524@gmail.com>
2021-04-20 12:58:41 +0800

    add delete

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
-  document.querySelector('ul').addEventListener('click', function(event){
-    event.target.classList.toggle('checked')
-  })
+  const list = document.querySelectorAll('li')
+  const span = document.querySelectorAll('li > span')
+  
+  list.forEach(ele => ele.addEventListener('click', function(event){
+    event.target.classList.toggle('checked') 
+  }))
+
+  span.forEach(ele => ele.addEventListener('click', function(event){
+    event.target.parentNode.remove()
+  }))
+
 })
