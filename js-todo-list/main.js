14380ffe1bafe99a7c98e8bc301cbff9fb605ff1
Vincent Yu <vinceyu1228@gmail.com>
2021-04-20 11:49:09 +0800

    delete function finished

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,15 @@
 // TO DO
+document.addEventListener('DOMContentLoaded', () => {
+  //const close = document.querySelectorAll('.close')
+
+  //close.map.addEventListener('click', function(){
+  //  console.log(this)
+  //  console.log(this.parentNode.parentNode);
+  //})
+
+  const ul = document.querySelector('ul')
+
+  ul.addEventListener('click', (e)=> {
+    ul.removeChild(e.target.parentNode)
+  })
+})
